ffd5b4d0c982bd8264c9848e338661e25ca3946c
Andrew Harvey <andrew@alantgeo.com.au>
2019-09-07 14:30:16 +1000

    origin and destination

--- www/src/App.vue
+++ www/src/App.vue
@@ -21,37 +21,37 @@
             :bottom="menuBottom"
             >
             <v-list dense>
-                <v-list-tile @click="setTripOrigin">
-                    <v-list-tile-avatar>
+                <v-list-item @click="setTripOrigin">
+                    <v-list-item-icon>
                         <v-icon>trip_origin</v-icon>
-                    </v-list-tile-avatar>
-                    <v-list-tile-content>
-                        <v-list-tile-title>From</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-                <v-list-tile @click="setTripWaypoint">
-                    <v-list-tile-avatar>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title>From</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item @click="setTripWaypoint">
+                    <v-list-item-avatar>
                         <v-icon>add_circle</v-icon>
-                    </v-list-tile-avatar>
-                    <v-list-tile-content>
-                        <v-list-tile-title>Via</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-                <v-list-tile @click="setTripDestination">
-                    <v-list-tile-avatar>
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                        <v-list-item-title>Via</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item @click="setTripDestination">
+                    <v-list-item-icon>
                         <v-icon>place</v-icon>
-                    </v-list-tile-avatar>
-                    <v-list-tile-content>
-                        <v-list-tile-title>To</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title>To</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
             </v-list>
         </v-menu>
         <div id="panel">
         <v-container>
             <v-row no-gutters>
                 <v-text-field
-                    v-model="from"
+                    v-model="origin"
                     label="From"
                     prepend-icon="trip_origin"
                     solo
@@ -59,7 +59,7 @@
             </v-row>
             <v-row no-gutters>
                 <v-text-field
-                    v-model="to"
+                    v-model="destination"
                     label="To"
                     prepend-icon="room"
                     solo
@@ -73,7 +73,7 @@
                   <v-btn-toggle v-model="mode" mandatory>
                       <v-tooltip bottom>
                           <template v-slot:activator="{ on }">
-                              <v-btn flat v-on="on" value="cycling">
+                              <v-btn v-on="on" value="cycling">
                                   <v-icon>directions_bike</v-icon>
                               </v-btn>
                           </template>
@@ -81,7 +81,7 @@
                       </v-tooltip>
                       <v-tooltip bottom>
                           <template v-slot:activator="{ on }">
-                              <v-btn flat v-on="on" value="walking">
+                              <v-btn v-on="on" value="walking">
                                   <v-icon>directions_walk</v-icon>
                               </v-btn>
                           </template>
@@ -90,7 +90,6 @@
                   </v-btn-toggle>
               </v-flex>
           </v-layout>
-
         </v-container>
         </div>
         <div id="map"></div>
@@ -114,6 +113,8 @@ export default {
         origin: null,
         destination: null,
         waypoints: [],
+
+        mode: 0,
 
         // show map click menu
         menu: false,
@@ -139,10 +140,77 @@ export default {
             zoom: 10,
             hash: true
         })
-
         this.map.on('load', this.mapLoaded);
+
+        const originSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+        originSVG.setAttributeNS(null, 'display', 'block')
+        originSVG.setAttributeNS(null, 'width', '20px')
+        originSVG.setAttributeNS(null, 'height', '20px')
+        originSVG.setAttributeNS(null, 'viewBox', '0 0 20 20')
+
+        const originSVGCircle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+        originSVGCircle.setAttributeNS(null, 'r', '8.5')
+        originSVGCircle.setAttributeNS(null, 'cy', '10')
+        originSVGCircle.setAttributeNS(null, 'cx', '10')
+        originSVGCircle.setAttributeNS(null, 'fill', this.$vuetify.theme.themes.light.primary)
+        originSVGCircle.setAttributeNS(null, 'stroke', '#ffffff')
+        originSVGCircle.setAttributeNS(null, 'stroke-width', '2.0')
+
+        originSVG.appendChild(originSVGCircle)
+
+        this.originMarker = new mapboxgl.Marker({
+            element: originSVG,
+            draggable: true
+        })
+
+        this.destinationMarker = new mapboxgl.Marker({
+            color: this.$vuetify.theme.themes.light.primary,
+            draggable: true
+        })
+
+        this.originMarker.on('dragend', () => {
+            this.origin = this.originMarker
+                .getLngLat()
+                .toArray()
+                .map(coord => {
+                    return Number(coord.toFixed(5))
+                })
+        })
+        this.destinationMarker.on('dragend', () => {
+            this.destination = this.destinationMarker
+                .getLngLat()
+                .toArray()
+                .map(coord => {
+                    return Number(coord.toFixed(5))
+                })
+        })
+    },
+    watch: {
+        origin: function (origin) {
+            this.originMarker
+                .setLngLat(origin)
+                .addTo(this.map);
+
+            this.updateDirections()
+
+            urlParams.set('origin', origin.join(','))
+            this.updateUrl()
+        },
+        destination: function (destination) {
+            this.destinationMarker
+                .setLngLat(destination)
+                .addTo(this.map);
+
+            this.updateDirections()
+
+            urlParams.set('destination', destination.join(','))
+            this.updateUrl()
+        }
     },
     methods: {
+        updateUrl: () => {
+            history.pushState(null, '', window.location.pathname + '?' + urlParams.toString())
+        },
         mapLoaded: function () {
             this.map.on('contextmenu', this.contextClick)
 
@@ -151,6 +219,20 @@ export default {
             }
             if (urlParams.has('destination')) {
                 this.destination = urlParams.get('destination').split(',').map(Number)
+            }
+        },
+        setTripOrigin: function () {
+            this.origin = this.menuLL
+        },
+        setTripWaypoint: function () {
+            this.waypoints.push(this.menuLL)
+        },
+        setTripDestination: function () {
+            this.destination = this.menuLL
+        },
+        updateDirections: function () {
+            if (this.origin && this.destination) {
+                this.foo = true
             }
         },
         contextClick: function (e) {
